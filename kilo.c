0ca9f30e9e0bd0cd3749f80826ea381b8d5f260e
Pranava116 <pranavagrao116@gmail.com>
2025-12-27 09:42:34 +0530

    feat: formatting and enums chore: escape sequence not readable

--- kilo.c
+++ kilo.c
@@ -9,17 +9,27 @@
 #define CTRL_KEY(k) ((k) & 0x1f)
 #define AUBF_INIT {NULL, 0}
 // ------------data----------
+typedef struct erow{
+	int size;
+	char *chars;
 
+}erow;
 enum editorKey {
   ARROW_LEFT = 1000,
   ARROW_RIGHT ,
   ARROW_UP,
-  ARROW_DOWN
+  ARROW_DOWN,
+  PAGE_UP,
+  PAGE_DOWN,
+  HOME_KEY,
+  END_KEY
 };
 struct editorConfig{
   int cx, cy;
   int screenRows;
   int screenCols;
+  int numows;
+  erow row;
   struct termios orig_termios;
 };
 struct editorConfig E;
@@ -89,63 +99,109 @@ void abFree(struct aubf *ab){
   free(ab->b);
 }
 int editorReadKey(){
-  int nread;
+	  int nread;
   char c;
-  while((nread = read(STDIN_FILENO, &c, 1))!=1){
-    if(nread == -1 && errno != EAGAIN) die("read");
+  while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+    if (nread == -1 && errno != EAGAIN) die("read");
   }
-  if(c == '\x1b'){
+  if (c == '\x1b') {
     char seq[3];
-
     if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
     if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
     if (seq[0] == '[') {
-      switch (seq[1]) {
-        case 'A': return ARROW_UP;
-        case 'B': return ARROW_DOWN;
-        case 'C': return ARROW_RIGHT;
-        case 'D': return ARROW_LEFT;
+      if (seq[1] >= '0' && seq[1] <= '9') {
+        if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+        if (seq[2] == '~') {
+          switch (seq[1]) {
+		case '1': return HOME_KEY;
+		case '4': return END_KEY;
+		case '8': return END_KEY;
+		case '7': return HOME_KEY;
+            	case '5': return PAGE_UP;
+            	case '6': return PAGE_DOWN;
+          }
+        }
+      } else {
+        switch (seq[1]) {
+          case 'A': return ARROW_UP;
+          case 'B': return ARROW_DOWN;
+          case 'C': return ARROW_RIGHT;
+          case 'D': return ARROW_LEFT;
+	
+          case 'H': return HOME_KEY;
+          case 'F': return END_KEY;
+        }
       }
     }
+      else if(seq[0] == '0'){
+      	switch(seq[1]){
+		case 'H': return HOME_KEY;
+		case 'F': return END_KEY;
+	}
+}
     return '\x1b';
-  }
-  else{
-  return c;
-  }
-}
-
-void editorMoveCursor(int c){
-  switch(c){
-    case ARROW_UP: E.cy--;
-    break;
-    case ARROW_LEFT: E.cx--;
-
-    break;
-    case ARROW_DOWN: E.cy++;
-    break;
-    case ARROW_RIGHT: E.cx++;
-    break;
+  } else {
+    return c;
   }
 }
 
+void editorMoveCursor(int key){
+  switch (key) {
+    case ARROW_LEFT:
+      if (E.cx != 0) {
+        E.cx--;
+      }
+      break;
+    case ARROW_RIGHT:
+      if (E.cx != E.screenCols - 1) {
+        E.cx++;
+      }
+      break;
+    case ARROW_UP:
+      if (E.cy != 0) {
+        E.cy--;
+      }
+      break;
+    case ARROW_DOWN:
+      if (E.cy != E.screenRows - 1) {
+        E.cy++;
+      }
+      break;
+  }
+}
 void editorProcessKeyPress(){
-  int c= editorReadKey();
+int c = editorReadKey();
+printf("KEy : %d\r\n" ,c);
   switch (c) {
     case CTRL_KEY('q'):
-  write(STDIN_FILENO, "\x1b[2J", 4);
-  write(STDIN_FILENO, "\x1b[H", 3);
+      write(STDOUT_FILENO, "\x1b[2J", 4);
+      write(STDOUT_FILENO, "\x1b[H", 3);
       exit(0);
       break;
+    case HOME_KEY:
+      E.cx = 0;
+      break;
+    case END_KEY:
+      E.cx = E.screenCols -1;
+     break;
+      break;
+    case PAGE_UP:
+    case PAGE_DOWN:
+      {
+	      int times = E.screenRows;
+	      while(times--){
+		      editorMoveCursor(c == PAGE_UP?ARROW_UP:ARROW_DOWN);
+	      }
+	      break;
+      }
     case ARROW_UP:
     case ARROW_DOWN:
     case ARROW_LEFT:
     case ARROW_RIGHT:
-    editorMoveCursor(c);
-    break;
-  }
-
+      editorMoveCursor(c);
+      break;
+  }	
 }
-
 //------------output-----------------
 
 void editorDrawRows(struct aubf *ab){
